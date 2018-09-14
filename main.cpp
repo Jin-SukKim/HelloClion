b2e3d0d269debc9bb6ec41e4a4b162f72a63b2d7
Jin-Suk Kim <99wlstjr@gmail.com>
2018-09-14 15:05:16 -0700

    only positive num

--- main.cpp
+++ main.cpp
@@ -7,8 +7,12 @@ int gcd(const int a, const int b) {
 int main() {
     int a, b;
 
-    cout << "Enter two numbers: " << endl;
+    cout << "Enter two numbers : " << endl;
     cin >> a >> b;
+    while (a<0 || b<0) {
+        cout << "Enter two positive numbers : " << endl;
+        cin >> a >> b;
+    }
     cout << "Greatest Common Denoinator is : " << gcd(a, b) << endl;
     return 0;
 }
