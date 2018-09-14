1372d2c7150113a41439c299887665c806f233c2
Jin-Suk Kim <99wlstjr@gmail.com>
2018-09-14 14:59:17 -0700

    modify ouput

---
+++ gcd.hpp
@@ -0,0 +1,18 @@
+//
+// Created by user on 9/14/2018.
+//
+
+#ifndef HELLOCLION_GCD_HPP
+#define HELLOCLION_GCD_HPP
+
+
+// Greatest Common Denominator
+// PRE : a is a positive integer
+// PRE : b is a positive integer
+// POST : a and b are unchanged
+// RETURN : the greatest common denominator of
+// a and b.
+int gcd ( const int a, const int b);
+
+
+#endif //HELLOCLION_GCD_HPP
